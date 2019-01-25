9d51403612d69fb42041d3d9ba60b7851dd377da
svs-odoo <svs@odoo.com>
2019-01-25 13:33:02 +0100

    12.0-20 - 2.1B

--- awesome_tshirt/static/src/js/image_preview_widget.js
+++ awesome_tshirt/static/src/js/image_preview_widget.js
@@ -2,20 +2,32 @@ odoo.define('awesome_tshirt.preview_widget', function (require) {
 "use strict";
 
 var basic_fields = require('web.basic_fields');
+var core = require('web.core');
 var field_registry = require('web.field_registry');
 
+var _t = core._t;
 var FieldChar = basic_fields.FieldChar;
 
 
 var ImagePreviewWidget = FieldChar.extend({
+
+    isSet: function () {
+        return true;
+    },
 
     // -------------------------------------------------------------------------
     // Private
     // -------------------------------------------------------------------------
 
     _renderReadonly: function () {
-        if (this.isSet()) {
-            this.$el.html($('<img>', {src: this.value}));
+        if (this.value) {
+            this.$el.html($('<img>', {
+                src: this.value,
+                style: 'max-width: 300px',
+            }));
+        } else {
+            this.$el.text(_t('MISSING TSHIRT DESIGN'));
+            this.$el.addClass('alert-danger');
         }
     },
 });
