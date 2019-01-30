f455437c6c020be3b8c7b0eb0e6c836ff60b76c2
svs-odoo <svs@odoo.com>
2019-01-30 14:10:02 +0100

    12.0-20 - 2.7.2

--- awesome_tshirt/static/src/js/home_menu.js
+++ awesome_tshirt/static/src/js/home_menu.js
@@ -8,14 +8,43 @@ var _t = core._t;
 
 
 HomeMenu.include({
+
+    /**
+     * @override
+     */
+    willStart: function () {
+        var self = this;
+        this._super.apply(this, arguments);
+
+        this.textMessage = _t('Yipi-kee-aye, mother-father !');
+
+        var def = this._rpc({
+            route: '/awesome_tshirt/bafienistalkingtoyou',
+        });
+        def.then(function (receivedText) {
+            self.textMessage = receivedText;
+        });
+
+        return def;
+    },
+
+
+    // -------------------------------------------------------------------------
+    // Private
+    // -------------------------------------------------------------------------
+
+    /**
+     * @override
+     */
     _render: function () {
         this._super.apply(this, arguments);
-        var message = $('<div>');
-        message.addClass('o_custom_message alert-info');
-        message.text(_t('Bafien Ckinpaers is watching you!'));
+
+        var $message = $('<div>');
+        $message.addClass('o_custom_message alert-info');
+        $message.text(this.textMessage);
 
         this.$('.o_custom_message').remove();
-        message.prependTo(this.$el);
+        $message.prependTo(this.$el);
     }
 });
 
